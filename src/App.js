11840b7169b16f56e787b2ea4fdb30a793e6af75
BlindException <robertprock31@gmail.com>
2023-07-15 19:29:12 -0400

    Added sortable columns

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import React, { useEffect, useState } from 'react';
+
+
 import axios from 'axios';
+
+
 import NFLDropDown from './NFLDropDown';
+
+
 import 'bootstrap/dist/js/bootstrap.min.js';
+
+
 function App() {
+
+
 const [players, setPlayers] = useState([]);
+
+
 const [leagueType, setLeagueType] = useState('');
+
+
 const [week, setWeek] = useState('');
+
+
 const [team, setTeam] = useState('');
 const [position, setPosition] = useState('');
 const [searchValue, setSearchValue] = useState('');
 const [searchTimeout, setSearchTimeout] = useState(null);
+const [sortedBy, setSortedBy] = useState('starter_count');
+const [sortOrder, setSortOrder] = useState('desc');
 useEffect(() => {
 fetchData();
-}, [leagueType, week, team, position, searchValue]);
+}, [leagueType, week, team, position, searchValue, sortedBy, sortOrder]);
 useEffect(() => {
 fetchData();
 }, []);
@@ -22,19 +40,50 @@ const response = await axios.get('https://robertprockjr.com/mfl_v2/', {
 mode: "cors",
 params: {
 leagueType: leagueType,
+
+
 week: week,
+
+
 team: team,
+
+
 position: position,
+
+
 search: searchValue,
+
+
+sortedBy: sortedBy,
+
+
+sortOrder: sortOrder,
+
+
 },
+
+
 });
+
+
 const data = response.data;
 
+
 if (data.length === 0) {
+
+
 setPlayers([
+
+
 {
+
+
 id: 'NoData',
+
+
 name: 'No player data available.',
+
+
 },
 ]);
 } else {
@@ -47,18 +96,66 @@ console.error(error);
 const handleTeamChange = (selectedTeam) => {
 setTeam(selectedTeam);
 };
+
+
 const handleSearchChange = (e) => {
+
+
 const value = e.target.value;
+
+
 setSearchValue(value);
+
+
 if (searchTimeout) {
+
+
 clearTimeout(searchTimeout);
+
+
 }
+
+
 setSearchTimeout(
+
+
 setTimeout(() => {
+
+
 fetchData();
+
+
 }, 3000)
+
+
 );
+
+
 };
+const handleSort = (column) => {
+
+
+    if (sortedBy === column) {
+    
+    
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+    
+    
+    } else {
+    
+    
+    setSortedBy(column);
+    
+    
+    setSortOrder('asc');
+    
+    
+    }
+    
+    
+    };
+
+
 return (
 <div className="container">
 <h1 className="text-center mt-5">Starting Players</h1>
@@ -67,101 +164,321 @@ return (
 <label htmlFor="leagueType">League Type:</label>
 <select
 id="leagueType"
+
+
 value={leagueType}
+
+
 onChange={(e) => setLeagueType(e.target.value)}
+
+
 className="form-control"
+
+
 >
+
+
 <option value="">ALL</option>
+
+
 <option value="Basic">Basic</option>
+
+
 <option value="Deluxe">Deluxe</option>
+
+
 <option value="Premium">Premium</option>
+
+
 </select>
+
+
 </div>
+
+
 <div className="col-md-6">
+
+
 <label htmlFor="week">Week:</label>
+
+
 <select
+
+
 id="week"
+
+
 value={week}
+
+
 onChange={(e) => setWeek(e.target.value)}
+
+
 className="form-control"
+
+
 >
+
+
 <option value="">ALL</option>
+
+
 {Array.from({ length: 18 }, (_, i) => i + 1).map((week) => (
+
+
 <option key={week} value={week}>
+
+
 {week}
+
+
 </option>
+
+
 ))}
+
+
 </select>
+
+
 </div>
+
+
 </div>
+
+
 <div className="row">
+
+
 <div className="col-md-6">
+
+
 <label htmlFor="team">Team:</label>
+
+
 <NFLDropDown onChange={handleTeamChange} />
+
+
 </div>
+
+
 <div className="col-md-6">
+
+
 <label htmlFor="position">Position:</label>
+
+
 <select
+
+
 id="position"
+
+
 value={position}
+
+
 onChange={(e) => setPosition(e.target.value)}
+
+
 className="form-control"
+
+
 >
+
+
 <option value="">ALL</option>
+
+
 <option value="QB">QB</option>
+
+
 <option value="RB">RB</option>
+
+
 <option value="WR">WR</option>
+
+
 <option value="TE">TE</option>
+
+
 </select>
+
+
 </div>
+
+
 </div>
+
+
 <div className="row">
+
+
 <div className="col-md-6">
+
+
 <label htmlFor="search">Search for player name:</label>
+
+
 <input
+
+
 type="search"
+
+
 id="search"
+
+
 value={searchValue}
+
+
 onChange={handleSearchChange}
+
+
 className="form-control"
+
+
 />
+
+
 </div>
+
+
 </div>
+
+
 <div className="row">
+
+
 <div className="col-md-12">
+
+
 <table className="table table-responsive">
+
+
 <thead>
+
+
 <tr>
-<th>Player Name</th>
-<th>Team</th>
-<th>Position</th>
-<th>Starts</th>
-<th>Non-Starts</th>
-<th>Starting %</th>
+
+
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('name')}
+>
+Player Name
+</button>
+</th>
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('team')}
+>
+Team
+</button>
+</th>
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('position')}
+>
+Position
+</button>
+</th>
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('starter_count')}
+>
+Starts
+</button>
+</th>
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('nonstarter_count')}
+>
+Non-Starts
+</button>
+</th>
+<th>
+<button
+className="btn btn-link"
+onClick={() => handleSort('starting_percent')}
+>
+Starting %
+</button>
+
+</th>
+
 </tr>
+
 </thead>
+
 <tbody>
+
 {players.length === 0 ? (
 <tr>
-<td>Loading data...</td>
+<td>Loading data...Please wait.</td>
+
 </tr>
+
+
 ) : (
+
+
 players.map((player) => (
+
+
 <tr key={player.id}>
+
+
 <td>{player.name}</td>
+
+
 <td>{player.team}</td>
+
+
 <td>{player.position}</td>
+
+
 <td>{player.starter_count}</td>
+
+
 <td>{player.nonstarter_count}</td>
+
+
 <td>{player.starting_percent}%</td>
+
+
 </tr>
+
+
 ))
+
+
 )}
+
+
 </tbody>
+
+
 </table>
+
+
 </div>
+
+
 </div>
+
+
 </div>
+
+
 );
+
+
 }
+
+
 export default App;
